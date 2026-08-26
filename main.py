9b3e07f7de7e4cf9388fb411db666d80cf3ce919
huzaifa-4 <huzaifamughal72830@gmail.com>
2026-08-27 01:54:17 +0500

    Stage 2: Add task read endpoints

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI",
+        "description": "Build my first CRUD API",
+        "completed": False
+    },
+    {
+        "id": 2,
+        "title": "Test API endpoints",
+        "description": "Test the CRUD operations",
+        "completed": False
+    }
+]
 
 
 @app.get("/")
@@ -11,3 +27,20 @@ def hello():
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )

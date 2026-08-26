d4e1d4665b4e1d1072a5bdaadfa67f144d14d6db
huzaifa-4 <huzaifamughal72830@gmail.com>
2026-08-27 01:57:48 +0500

    Stage 4: Add update and delete endpoints

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ class TaskCreate(BaseModel):
     completed: bool = False
 
 
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    completed: bool | None = None
+
+
 tasks = [
     {
         "id": 1,
@@ -67,3 +73,37 @@ def create_task(task: TaskCreate):
     tasks.append(new_task)
 
     return new_task
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task_update: TaskUpdate):
+    for task in tasks:
+        if task["id"] == task_id:
+            if task_update.title is not None:
+                task["title"] = task_update.title
+
+            if task_update.description is not None:
+                task["description"] = task_update.description
+
+            if task_update.completed is not None:
+                task["completed"] = task_update.completed
+
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+
+
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: int):
+    for index, task in enumerate(tasks):
+        if task["id"] == task_id:
+            tasks.pop(index)
+            return
+
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
